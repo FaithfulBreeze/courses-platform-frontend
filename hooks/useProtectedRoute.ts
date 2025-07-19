4e2edbac195081cc3fd8132b2801033e27922433
FaithfulBreeze <faithfulbreeze@outlook.com>
2025-07-19 11:56:38 -0300

    feat: improve auth by rotating token

--- hooks/useProtectedRoute.ts
+++ hooks/useProtectedRoute.ts
@@ -1,6 +1,25 @@
 import { IAuthContext } from "@/contexts/AuthContext";
 import { redirect } from "next/navigation";
+import { toast } from "sonner";
 
-export const useProtectedRoute = (context: IAuthContext) => {
-  if (!context.user.id) redirect("/auth");
+export const useProtectedRoute = async (context: IAuthContext) => {
+  if (!context.user.id) {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/refresh`,
+      {
+        credentials: "include",
+      }
+    );
+
+    const parsedResponse = await response.json();
+
+    if (!response.ok) {
+      toast(parsedResponse.message)
+      redirect("/auth");
+    }
+
+    if (!parsedResponse?.user) redirect("/auth");
+
+    context.setUser(parsedResponse.user);
+  }
 };
--- providers/AuthProvider.tsx
+++ providers/AuthProvider.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { AuthContext } from "@/contexts/AuthContext";
+import { User } from "@/types.generated";
 import { useState } from "react";
 
 export function AuthProvider({ children }: React.PropsWithChildren) {
-  const [user, setUser] = useState<{ id: string }>({ id: "" });
+  const [user, setUser] = useState<Partial<User>>({});
 
   return (
-    <AuthContext.Provider value={{ user: { id: user.id }, setUser }}>
+    <AuthContext.Provider value={{user, setUser}}>
       {children}
     </AuthContext.Provider>
   );
