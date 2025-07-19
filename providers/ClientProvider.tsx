0a4cdf0c8985e855e9e8ba433090b36b6df3079a
FaithfulBreeze <faithfulbreeze@outlook.com>
2025-07-19 15:53:47 -0300

    Fix: added post method in refresh route and include credentials in graphql client

--- providers/ClientProvider.tsx
+++ providers/ClientProvider.tsx
@@ -4,6 +4,6 @@ import { ClientContext } from '@/contexts/ClientContext';
 import { GraphQLClient } from 'graphql-request';
 
 export function ClientProvider({ children }: React.PropsWithChildren) {
-  const client = new GraphQLClient(`${process.env.NEXT_PUBLIC_BACKEND_URL}/graphql`);
+  const client = new GraphQLClient(`${process.env.NEXT_PUBLIC_BACKEND_URL}/graphql`, { credentials: 'include' });
   return <ClientContext.Provider value={{ client }}>{children}</ClientContext.Provider>;
 }
