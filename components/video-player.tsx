c2813f18abb4cb86bef63b6fda27014f9e95e26d
FaithfulBreeze <faithfulbreeze@outlook.com>
2025-07-31 21:17:50 -0300

    Feat: Videojs player for lessons page

---
+++ components/video-player.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useRef } from 'react';
+import videojs from 'video.js';
+import Player from 'video.js/dist/types/player';
+import 'video.js/dist/video-js.css';
+
+export const VideoJS = (props: any) => {
+  const videoRef = useRef<any>(null);
+  const playerRef = useRef<Player>(null);
+  const { options, onReady } = props;
+
+  useEffect(() => {
+    if (!playerRef.current) {
+      const videoElement = document.createElement('video-js');
+
+      videoElement.classList.add('vjs-big-play-centered');
+      if (videoRef.current) videoRef.current.appendChild(videoElement);
+
+      const player = (playerRef.current = videojs(videoElement, options, () => {
+        if (onReady) onReady(player);
+      }));
+    } else {
+      const player = playerRef.current;
+
+      player.autoplay(options.autoplay);
+      player.src(options.sources);
+    }
+  }, [options, videoRef]);
+
+  useEffect(() => {
+    const player = playerRef.current;
+
+    return () => {
+      if (player && !player.isDisposed()) {
+        player.dispose();
+        playerRef.current = null;
+      }
+    };
+  }, [playerRef]);
+
+  return (
+    <div data-vjs-player>
+      <div ref={videoRef} />
+    </div>
+  );
+};
+
+export default VideoJS;
